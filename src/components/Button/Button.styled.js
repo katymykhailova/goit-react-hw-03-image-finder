1f5bb4388d9c75e318d6d811065503a209329b0a
kotikm81 <77439323+kotikm81@users.noreply.github.com>
2021-07-05 21:13:26 +0300

    -

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -8,7 +8,7 @@ export const LoadLoreButton = styled.button`
   background-color: #3f51b5;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   text-align: center;
-  display: inline-block;
+  /* display: inline-block; */
   color: #fff;
   border: 0;
   text-decoration: none;
